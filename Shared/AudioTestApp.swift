48b0fd447d1db01eef72ece1078f139c8a91e7ba
shimma <shimma.dev@gmail.com>
2021-07-03 16:38:18 +0700

    Initial Commit

---
+++ Shared/AudioTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioTestApp.swift
+//  Shared
+//
+//  Created by Shuichi on 2021/07/03.
+//
+
+import SwiftUI
+
+@main
+struct AudioTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
